7afe262de564cd69576e2b95a526914c57298182
Mayank <mayankanand2701@gmail.com>
2024-03-02 15:52:17 +0530

    Adding the Complete Project code

---
+++ script.js
@@ -0,0 +1,59 @@
+const API_KEY="Your API Key here";
+const url="https://newsapi.org/v2/everything?q=";
+
+window.addEventListener("load",()=>fetchNews("India"));
+
+async function fetchNews(query){
+    const res=await fetch(`${url}${query}&apiKey=${API_KEY}`);
+    const data=await res.json();
+    bindData(data.articles);
+}
+function bindData(articles){
+    const cardContainer=document.getElementById("cards-container");
+    const newsCardTemplate=document.getElementById("template-news card");
+
+    cardContainer.innerHTML="";
+
+    articles.forEach(article => {
+        if (!article.urlToImage) return;
+        const cardsClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardsClone,article);
+        cardContainer.appendChild(cardsClone);
+    });
+}
+function fillDataInCard(cardsClone,article){
+    const newsImg=cardsClone.querySelector("#news-img");
+    const newsTitle=cardsClone.querySelector("#news-title");
+    const newsSource=cardsClone.querySelector("#news-source");
+    const newsDesc=cardsClone.querySelector("#news-desc");
+
+    newsImg.src=article.urlToImage;
+    newsTitle.innerHTML=article.title;
+    newsDesc.innerHTML=article.description;
+
+    const date=new Date(article.publishedAt).toLocaleString("en-US",{timeZone:"Asia/Jakarta"});
+    newsSource.innerHTML=`${article.source.name} . ${date}`;
+
+    cardsClone.firstElementChild.addEventListener("click",()=>{
+        window.open(article.url,"_blank");
+    })
+}
+let curSelectedNav=null;
+function onNavItemClick(id){
+    fetchNews(id);
+    const navItem=document.getElementById(id);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav=navItem;
+    curSelectedNav.classList.add("active");
+}
+
+const searchButton=document.getElementById("search-button");
+const serachText=document.getElementById("search-text");
+
+searchButton.addEventListener("click",()=>{
+    const query=serachText.value;
+    if(!query)return;
+    fetchNews(query);
+    curSelectedNav?.classList.remove("active");
+    curSelectedNav=null;
+})
